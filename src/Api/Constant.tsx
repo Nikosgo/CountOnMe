9f43c769f26087a50381709c86db7845c1f198f5
misterkillerman <74764482+misterkillerman@users.noreply.github.com>
2025-05-03 01:35:54 +0800

    update constants api

--- src/Api/Constant.tsx
+++ src/Api/Constant.tsx
@@ -1,8 +1,9 @@
 // uncomment to test locally
-const API_BASE_URL = 'http://localhost:8080';
-// const API_BASE_URL = "http://54.169.139.191:8080"
-export const TRANSACTIONS_BASE_URL = API_BASE_URL + "/transactions";
-export const USERS_BASE_URL = API_BASE_URL + "/users";
+// const API_BASE_URL = 'http://localhost:8080';
+const API_TRANSACTIONS_BASE_URL = "http://13.251.125.214:8000"
+const API_USERS_BASE_URL = "http://13.251.125.214:8080"
+export const TRANSACTIONS_BASE_URL = API_TRANSACTIONS_BASE_URL + "/transactions";
+export const USERS_BASE_URL = API_USERS_BASE_URL + "/users";
 
 export const USER_API = {
     BASE_URL: USERS_BASE_URL,
